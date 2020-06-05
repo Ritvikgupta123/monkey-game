db00ea84945f5011fa251b456c9f87f54cc1cf19
Ritvikgupta123 <66456550+Ritvikgupta123@users.noreply.github.com>
2020-06-05 18:50:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+ var ball, img, paddle;
+function preload() {
+ ballimg = loadImage("ball.png");
+ paddleimg = loadImage("paddle.png")
+}
+ function setup() {
+createCanvas(400, 400);
+ ball = createSprite(200, 200, 10, 10);
+ ball.addImage(ballimg);
+ ball.velocityX = -9;
+ paddle = createSprite(380, 200, 20, 100);
+ paddle.addImage(paddleimg)
+}
+function draw() {
+ background(205, 153, 0);
+edges = createEdgeSprites();
+//Bounce Off the Left Edge only
+    ball.bounceOff(edges[0]);
+    ball.bounceOff(edges[2]);
+   ball.bounceOff(edges[3]);
+ball.bounceOff(paddle);
+if (keyDown(UP_ARROW)) {
+ paddle.y = paddle.y - 20;
+}
+  if (keyDown(DOWN_ARROW)) {
+  paddle.y = paddle.y + 20;
+}
+  explosion();
+drawSprites();
+}
+function explosion() {
+ ball.velocityY = random(-8, 8);
+}
